73b2434ae10113edc7def5db6959abc51e253738
hupe1980 <hello@webappsolutions.de>
2019-05-26 20:10:17 +0200

    Misc

--- examples/using-redux/gatsby-config.js
+++ examples/using-redux/gatsby-config.js
@@ -2,15 +2,5 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Theme Material-UI`,
   },
-  __experimentalThemes: [
-    {
-      resolve: `gatsby-theme-material-ui`,
-      options: {
-        // If you want to use styled components you should change the injection order.
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
-      },
-    },
-  ],
+  __experimentalThemes: [`gatsby-theme-material-ui`],
 };
