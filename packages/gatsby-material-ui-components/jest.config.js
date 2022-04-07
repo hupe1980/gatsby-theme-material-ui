536c70b07ca7a0231a3f07fc80b5126671940e9e
Dan <35927536+dan-mba@users.noreply.github.com>
2022-04-07 09:32:53 -0400

    Update for React 18

--- packages/gatsby-material-ui-components/jest.config.js
+++ packages/gatsby-material-ui-components/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*test)\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1',
+  },
   testEnvironment: 'jsdom',
 };
