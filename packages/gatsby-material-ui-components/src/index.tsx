10eee43b9e09aefba6277d567db581a5ed654848
hupe1980 <frankhuebner1980@gmail.com>
2022-08-11 17:53:11 +0200

    Export button base

--- packages/gatsby-material-ui-components/src/index.tsx
+++ packages/gatsby-material-ui-components/src/index.tsx
@@ -3,6 +3,7 @@ export * from './list-item-button';
 export * from './glink';
 export * from './card-action-area';
 export * from './button';
+export * from './button-base';
 export * from './icon-button';
 export * from './fab';
 export * from './bottom-navigation-action';
